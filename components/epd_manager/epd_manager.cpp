c64ce9d44b4a4649862663a1f39d587d9620e0b7
Paolo Gatti <paolino1986@gmail.com>
2019-10-06 12:01:25 +0200

    added methods to handle screen orientation

--- components/epd_manager/epd_manager.cpp
+++ components/epd_manager/epd_manager.cpp
@@ -14,8 +14,9 @@
 
 static const char *TAG = "EPD MANAGER";
 
-static const int TOP_LINE_Y = 30;
-static const int BOTTOM_LINE_Y = EPD_WIDTH -30;
+static const int TOP_LINE_HEIGHT = 30;
+static const int SCREEN_WIDTH = EPD_HEIGHT;
+static const int SCREEN_HEIGHT = EPD_WIDTH;
 
 unsigned char image[EPD_WIDTH*EPD_HEIGHT/8];
 Paint paint(image, 0, 0);    // width should be the multiple of 8
@@ -31,30 +32,42 @@ static void epd_manager_clear() {
   epd.DisplayFrame();
 }
 
-static void epd_manager_apply() {
+static void epd_manager_set_paint(int width, int height, int clear_color) {
+  
+  paint.Clear(clear_color);
+  paint.SetWidth(height);
+  paint.SetHeight(width);
+}
 
-  epd.SetFrameMemory(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
-  epd.DisplayFrame();
+static void epd_manager_draw_paint(int x, int y) {
+  
+  int x_abs_coord = EPD_WIDTH - paint.GetWidth() - y;
+  int y_abs_coord = x;
+  epd.SetFrameMemory(paint.GetImage(), x_abs_coord , y_abs_coord, paint.GetWidth(), paint.GetHeight());
 }
 
 static void epd_manager_draw_grid() {
 
-  paint.DrawHorizontalLine(0, TOP_LINE_Y, EPD_HEIGHT, COLORED);
-
-  paint.DrawHorizontalLine(0, BOTTOM_LINE_Y, EPD_HEIGHT, COLORED);
+  epd_manager_set_paint(SCREEN_WIDTH, SCREEN_HEIGHT, UNCOLORED);
+  paint.DrawHorizontalLine(0, TOP_LINE_HEIGHT, SCREEN_WIDTH, COLORED);
+  paint.DrawVerticalLine(SCREEN_WIDTH - 132, 0, TOP_LINE_HEIGHT, COLORED);
+  epd_manager_draw_paint(0, 0);
 }
 
 static void epd_manager_draw_time(char *time) {
 
-  paint.DrawStringAt(0, 50, time, &Font40, COLORED);
+  epd_manager_set_paint(SCREEN_WIDTH, 40, UNCOLORED);
+  paint.DrawStringAt(0, 0, time, &Font40, COLORED);
+  epd_manager_draw_paint(0, SCREEN_HEIGHT / 2);
 }
 
 static void epd_manager_draw_date(char *date) {
-
-  int y = (TOP_LINE_Y - 16) / 2;
-  paint.DrawStringAt(180, y, date, &Font16, COLORED);
-
-  //epd.SetFrameMemory(calendar_image, 10, 10, 25, 25);
+  
+  int paint_width = 120;
+  int paint_height = 24;
+  epd_manager_set_paint(paint_width, paint_height, UNCOLORED);
+  paint.DrawStringAt(0, 8, date, &Font16, COLORED);
+  epd_manager_draw_paint(SCREEN_WIDTH - paint_width, 0);
 }
 
 void epd_manager_init() {
@@ -67,19 +80,15 @@ void epd_manager_init() {
   ESP_LOGI(TAG, "e-Paper initialized");
 
   epd_manager_clear();
-
+  paint.Clear(UNCOLORED);
   paint.SetRotate(ROTATE_90);
-  paint.SetWidth(EPD_WIDTH);
-  paint.SetHeight(EPD_HEIGHT);
 }
 
 void epd_manager_update(char *time, char *date) {
 
-  paint.Clear(UNCOLORED);
-  
   epd_manager_draw_grid();
   epd_manager_draw_date(date);
   epd_manager_draw_time(time);
   
-  epd_manager_apply();
+  epd.DisplayFrame();
 }
