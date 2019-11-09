1ffcdfabe7ab319bc9587c25f189ba38f72bce3d
Matteo Somensi <>
2019-11-09 13:33:28 +0100

    Try to set a ip to wifi ap

--- components/enrollment_manager/enrollment_manager.c
+++ components/enrollment_manager/enrollment_manager.c
@@ -31,8 +31,17 @@ static esp_err_t event_handler(void *ctx, system_event_t *event) {
 void enrollment_manager_start(wifi_event_callback_t wifi_callback) {
 	event_callback = wifi_callback;
 	ESP_LOGI(TAG, "configuring enrollment manager start...");
+    
+    tcpip_adapter_init();
 
-	tcpip_adapter_init();
+    ESP_ERROR_CHECK(tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP));
+	tcpip_adapter_ip_info_t info;
+    memset(&info, 0, sizeof(info));
+    IP4_ADDR(&info.ip, 192, 168, 1, 1);
+    IP4_ADDR(&info.gw, 192, 168, 1, 1);
+    IP4_ADDR(&info.netmask, 255, 255, 255, 0);
+    ESP_ERROR_CHECK(tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_AP, &info)); 
+    ESP_ERROR_CHECK(tcpip_adapter_dhcps_start(TCPIP_ADAPTER_IF_AP));
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&cfg);
@@ -50,7 +59,7 @@ void enrollment_manager_start(wifi_event_callback_t wifi_callback) {
     };
 
     esp_wifi_set_mode(WIFI_MODE_AP);
-    esp_wifi_set_config(WIFI_MODE_APSTA, &wifi_config);
+    esp_wifi_set_config(WIFI_MODE_AP, &wifi_config);
     esp_wifi_start();
 
 	ESP_LOGI(TAG, "configuring enrollment manager end...");
