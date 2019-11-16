ea4554243cd954d6560a676bd317e5e5d0524a6e
Paolo Gatti <paolino1986@gmail.com>
2019-11-16 10:22:15 +0100

    fix random number generation

--- components/utils/utils.c
+++ components/utils/utils.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include "utils.h"
 
+#define DR_REG_RNG_BASE 0x3ff75144
+
 static char *esp_ssid_prefix =  "ESP-";
 
 const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -19,19 +21,19 @@ static void utils_generate_random_string(char *dst, int size, int offset) {
 }
 
 void utils_generate_random_pwd(char *dst, int size) {
-    srand(xTaskGetTickCount());
+    srand(READ_PERI_REG(DR_REG_RNG_BASE));
     utils_generate_random_string(dst, size, 0);
 }
 
 void utils_generate_random_ssid(char *dst, int size) {
 
     strncpy(dst, esp_ssid_prefix, 4);
-    srand(xTaskGetTickCount());
+    srand(READ_PERI_REG(DR_REG_RNG_BASE));
     utils_generate_random_string(dst, size, 4);
 }
 
 int utils_generate_random_token() {
 
-    srand(xTaskGetTickCount());
+    srand(READ_PERI_REG(DR_REG_RNG_BASE));
     return rand() % 1000000;
 }
